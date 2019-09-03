c34c0b7893f0966032c8a1b53aec503327f572e8
Daniella Cocco <d@daniellacoc.co>
2019-09-03 12:55:06 +0200

    Remove unused getInitalProps (enables automatic prerendering)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import App, { Container } from "next/app";
-// Fetch is used in an example which can be commented out.
-import fetch from 'isomorphic-unfetch'; // eslint-disable-line
 import { createGlobalStyle } from "styled-components";
 import { PageTransition } from "next-page-transitions";
 import LoaderBar, { loaderBarStyles } from "../components/LoaderBar";
@@ -56,72 +54,15 @@ const RecordRigStyling = createGlobalStyle`
 `;
 
 /**
- * Get app-wide data.
- */
-const getAppData = async () => {
-  /**
-   * Shows fetch use - replace with e.g. actual product data when available.
-   * Currently an example stub.
-   */
-  // if (process.env.NODE_ENV === 'production') {
-  //   const res = await fetch('https://api.github.com/repos/zeit/next.js');
-  //   const json = await res.json();
-  //   return { stars: json.stargazers_count };
-  // }
-  /** In development, do not fetch but use mock data instead. */
-  const appData = require('../data/mockAppData'); // eslint-disable-line
-  return appData;
-};
-
-/** Helper to determine runtime environment. */
-const isServer = typeof window === "undefined";
-
-/** Instantiate an empty client-side store (or not). */
-const clientStore = isServer ? null : {};
-
-/**
  * Overrides Next.js' default `App` component which is used for page initialization.
  * `Component` refers to the active page component. Pages can access and pass down `appData`.
  */
 export default class RecordRigApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    /** Set appData from clientStore (exists if fetched previously) or execute the fetch(es) now. */
-    const appData = clientStore ? clientStore.appData : await getAppData();
-
-    let pageProps = {};
-
-    /** If the page component has an `getInitialProps` method defined, we'll execute it. */
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { appData, pageProps };
-  }
-
-  constructor(props) {
-    super(props);
-
-    /** Fill clientStore with appData from initialProps (if it was instantiated). */
-    if (clientStore && !clientStore.appData) {
-      clientStore.appData = props.appData;
-    }
-  }
-
   render() {
     const {
-      /**
-       * App-wide data which is defined at the root and also fetched
-       * from the root `RecordRigApp` component.
-       */
-      appData,
       /** The "Component" component refers to whatever the currently active page component is. */
       Component,
       /** Used to set a unique key on the page component based on the current route. */
-      router,
-      /**
-       * Page-specific data, the fetches of which are defined along with the page
-       * and executed from the root `RecordRigApp` component.
-       */
       pageProps
     } = this.props;
 
@@ -133,7 +74,7 @@ export default class RecordRigApp extends App {
           skipInitialTransition
           timeout={200}
         >
-          <Component appData={appData} key={router.route} {...pageProps} />
+          <Component {...pageProps} />
         </PageTransition>
         {/**
          * LoaderBar manages its own lifecycle based on Next's Router and as such
