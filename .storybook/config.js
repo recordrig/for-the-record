4ad6562653b3f094c3a63b4c1070542fee5045ff
Daniella Cocco <d@daniellacoc.co>
2019-09-05 11:29:39 +0200

    Improve story comments

--- .storybook/config.js
+++ .storybook/config.js
@@ -39,19 +39,20 @@ addDecorator(withInfo);
  */
 addDecorator(withRecordRigFontStyles);
 
+/**
+* `require.context()` is a special feature supported by webpack's compiler that allows you to get
+* all matching modules starting from some base directory. The intention is to tell webpack at
+* compile time to transform that expression into a dynamic list of all the possible matching
+* module requests that it can resolve, in turn adding them as build dependencies and allowing
+* you to require them at runtime.
+* 
+* See [Explanation on Stackoverflow](https://stackoverflow.com/a/54066904) and/or
+* [Webpack's documentation on the feature](https://webpack.js.org/guides/dependency-management/#require-context)
+*/
 const req = require.context('../components', true, /\.stories\.tsx$/);
 
 /**
- * Import all `.stories` files dynamically using Webpack's `require.context()`.
- * 
- * `require.context()` is a special feature supported by webpack's compiler that allows you to get
- * all matching modules starting from some base directory. The intention is to tell webpack at
- * compile time to transform that expression into a dynamic list of all the possible matching
- * module requests that it can resolve, in turn adding them as build dependencies and allowing
- * you to require them at runtime.
- * 
- * See [Explanation on Stackoverflow](https://stackoverflow.com/a/54066904) and/or
- * [Webpack's documentation on the feature](https://webpack.js.org/guides/dependency-management/#require-context)
+ * Import all `.stories` files dynamically.
  */
 const loadStories = () => req.keys().forEach(req);
 
