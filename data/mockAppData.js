88064b605c7d669d9de6e4636539f6eeda764202
Daniella Cocco <d@daniellacoc.co>
2019-09-05 18:20:55 +0200

    Get rid of unused data folder

--- data/mockAppData.js
+++
@@ -1,4 +0,0 @@
-import * as mockAppData from "../.cypress/fixtures/appData.json";
-
-// Whatever data our testing tool Cypress uses, we'll also use as generic mock data.
-export default mockAppData;
