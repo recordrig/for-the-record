d40449244a3d90d61b0beb2d10429db1849c5776
Daniella Cocco <d@daniellacoc.co>
2020-04-15 16:34:38 +0200

    Fix Drawer page glitch on first open cause by Styled Components re-requesting fonts (required Next update to import global css)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,86 +5,14 @@ import withRedux from "next-redux-wrapper";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
-import { createGlobalStyle } from "styled-components";
 import rootReducer from "../store/_rootReducer";
 import Footer from "../components/Footer";
-import LoaderBar, { loaderBarStyles } from "../components/LoaderBar";
+import LoaderBar from "../components/LoaderBar";
 import ConnectedMenuBar from "../components/MenuBarContainer";
-
-/**
- * These styles may be assumed to be included on all pages, so that font availability and rendering
- * is guaranteed to be consistent. Additionally, this constant is exported so that utilities outside
- * the main application, e.g. Storybook, can import and use these values as well.
- */
-export const fontStyles = `
-  @font-face {
-    font-family: "IBM Plex Sans";
-    font-style: normal;
-    font-weight: 400;
-    src: url("/IBMPlexSans-Regular.woff") format("woff");
-  }
-
-  @font-face {
-    font-family: "IBM Plex Sans";
-    font-style: normal;
-    font-weight: 700;
-    src: url("/IBMPlexSans-Bold.woff") format("woff");
-  }
-
-  body {
-    font-family: "IBM Plex Sans", sans-serif;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-  }
-`;
-
-/**
- * The module `<PageTransition>` component needs these global classes to do its magic. This is
- * an external dependency (`next-page-transitions`) and as such, we do not have a way to circumvent
- * this requirement (unless we decide to adopt & modify the module).
- */
-const pageTransitionStyles = `
-  .page-transition-enter {
-    opacity: 0;
-  }
-
-  .page-transition-enter-active {
-    opacity: 1;
-    transition: opacity 200ms;
-  }
-
-  .page-transition-exit {
-    opacity: 1;
-  }
-
-  .page-transition-exit-active {
-    opacity: 0;
-    transition: opacity 200ms;
-  }
-`;
-
-/**
- * Styling defaults aiding consistency. May be overridden at the component level.
- */
-const ApplicationStyles = createGlobalStyle`
-  body {
-    background-color: #f2f4f8;
-    margin: 0;
-  }
-
-  ${fontStyles}
-
-  ${pageTransitionStyles}
-
-  ${loaderBarStyles}
-`;
+import "./_appStyles.css";
 
 /*
  * If in prod, initialize Sentry, our error logging tool.
- *
- * We import it conditionally because other tools importing from this file might break otherwise.
- * For example, Storybook imports styles from this file but its Webpack config isn't prepared for
- * Sentry, which it'll try to initialise nevertheless.
  */
 if (process.env.NODE_ENV === "production") {
   const Sentry = require("@sentry/node"); // eslint-disable-line
@@ -194,7 +122,6 @@ class RecordRigApp extends App {
          */}
         <LoaderBar />
         <ConnectedMenuBar />
-        <ApplicationStyles />
       </Provider>
     );
   }
