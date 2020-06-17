5d38c9f040f91985f06bb38c5aeb91dd7baeead6
Daniella Cocco <d@daniellacoc.co>
2020-06-17 09:28:16 +0200

    Shopping bag validators

--- data/products.ts
+++ data/products.ts
@@ -1,4 +1,5 @@
 interface Product {
+  readonly name: string;
   /** Price in cents. */
   readonly price: number;
 }
@@ -10,9 +11,11 @@ type Products = Record<ProductIds, Product>;
 
 const products: Products = {
   "RR20-black": {
+    name: "RecordRig - Stealth Black",
     price: 229900
   },
   "RR20-white": {
+    name: "RecordRig - Pristine White",
     price: 229900
   }
 };
---
+++ utils/shoppingBag.ts
@@ -0,0 +1,83 @@
+import productsData from "../data/products";
+import { sumTotal } from "./prices";
+
+interface Product {
+  readonly id: string;
+  readonly quantity: number;
+}
+
+type ErrorIds = "totalPrice" | "productQuantity";
+
+interface QuantityError {
+  readonly id: ErrorIds;
+  readonly description: string;
+  readonly product: string;
+}
+
+export const checkProductQuantities = (
+  products: readonly Product[]
+): {
+  readonly quantitiesAreValid: boolean;
+  readonly errors: readonly QuantityError[];
+} => {
+  const productsWhichExceed = products.filter(product => product.quantity > 4);
+  if (productsWhichExceed.length === 0)
+    return { quantitiesAreValid: true, errors: [] };
+  const errors: readonly QuantityError[] = productsWhichExceed.map(product => {
+    return {
+      id: "productQuantity",
+      description: `The product quantity for ${
+        productsData[product.id].name
+      } exceeds 4. Modify the amount for this products so that it is set between 1 and 4.`,
+      product: product.id
+    };
+  });
+  return {
+    quantitiesAreValid: false,
+    errors
+  };
+};
+
+interface TotalPriceError {
+  readonly id: ErrorIds;
+  readonly description: string;
+}
+
+export const checkTotalPrice = (
+  products: readonly Product[]
+): {
+  readonly totalPriceIsValid: boolean;
+  readonly errors: readonly TotalPriceError[];
+} => {
+  const prices = products.map(product => productsData[product.id].price);
+  const total = sumTotal(prices);
+  if (total <= 10000) return { totalPriceIsValid: true, errors: [] };
+  return {
+    totalPriceIsValid: false,
+    errors: [
+      {
+        id: "totalPrice",
+        description:
+          "The total price for all products exceeds the €10.000 threshold. Modify your shopping bag contents so that the total price remains below this amount."
+      }
+    ]
+  };
+};
+
+/**
+ * Checks if Shopping Bag is ok to check out.
+ */
+export const validateShoppingBag = (
+  products: readonly Product[]
+): {
+  readonly shoppingBagIsValid: boolean;
+  readonly errors: readonly (QuantityError | TotalPriceError)[];
+} => {
+  const quantitiesResult = checkProductQuantities(products);
+  const priceResult = checkTotalPrice(products);
+  return {
+    shoppingBagIsValid:
+      quantitiesResult.quantitiesAreValid && priceResult.totalPriceIsValid,
+    errors: [...quantitiesResult.errors, ...priceResult.errors]
+  };
+};
