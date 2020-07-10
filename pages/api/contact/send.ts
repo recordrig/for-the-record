a44c212f2ae523a6400dd0ebc604acdf9b7e31f1
Daniella Cocco <d@daniellacoc.co>
2020-07-10 19:37:38 +0200

    Contact: Send summary of message to sender

--- pages/api/contact/send.ts
+++ pages/api/contact/send.ts
@@ -5,7 +5,7 @@ export default async function(req, res) {
 
   const { name, email, subject, message } = req.body;
 
-  const content = {
+  const emailToSupport = {
     to: process.env.SENDGRID_TO_ADDRESS || "",
     from: {
       email: process.env.SENDGRID_FROM_ADDRESS || "", // Needs to be a verified email address or domain.
@@ -17,8 +17,29 @@ export default async function(req, res) {
     html: `<p>${message}</p>`
   };
 
+  const emailConfirmation = {
+    to: email,
+    from: {
+      email: process.env.SENDGRID_FROM_ADDRESS || "", // Needs to be a verified email address or domain.
+      name: "RecordRig"
+    },
+    replyTo: process.env.SENDGRID_FROM_ADDRESS,
+    subject: `Your message to RecordRig ("${subject}")`,
+    text: `Hi ${name}. You recently contacted RecordRig through the contact form. We should be able to get back to you within a couple of days. Here's a copy of your message: "${message}"`,
+    html: `<p>Hi ${name},</p>
+      <p>You recently contacted RecordRig through the contact form. We should be able to get back to you within a couple of days. Here's a copy of your email:</p>
+      <p>---</p>
+      <p>Subject: <strong>${subject}</strong></p>
+      <p>${message}</p>
+      <p>---</p>
+      <p>Speak soon,</p>
+      <p>RecordRig.</p>
+    `
+  };
+
   try {
-    await sgMail.send(content);
+    await sgMail.send(emailToSupport);
+    await sgMail.send(emailConfirmation);
     res.status(200).send("Message sent successfully.");
   } catch (error) {
     console.error(error);
